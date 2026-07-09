9b8e1e9bb32fa0b74d7fcf17930d9075c77b1a92
rajibmia1345885 <mdalikhan.ak47@gmail.com>
2026-07-09 23:02:04 +0600

    Update ReleaseVersion from OB53 to OB54

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ async def send_request(encrypted_uid, token, url):
             'Expect': "100-continue",
             'X-Unity-Version': "2018.4.11f1",
             'X-GA': "v1 1",
-            'ReleaseVersion': "OB53"
+            'ReleaseVersion': "OB54"
         }
         async with aiohttp.ClientSession() as session:
             async with session.post(url, data=edata, headers=headers) as response:
